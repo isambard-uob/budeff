6f7235ba9ede2d033e023330109037d840239f49
Chris Wells Wood <cwwoodesq@gmail.com>
2018-04-16 09:00:25 +0100

    Adds buff src and updates structure slightly.
    BUFF compiles and runs at this stage.

---
+++ src/buff/__init__.py
@@ -0,0 +1,4 @@
+from .force_field import BuffForceField, FORCE_FIELDS
+from .calculate_energy import (PyAtomData, score_interactions,
+                               get_within_ff_cutoff, find_inter_ampal,
+                               find_intra_ampal)
